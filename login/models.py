f034f4bf0df0f947f810a4e149230599eaac706a
Gabriel Borges <gabrielfr.borges@gmail.com>
2019-09-18 13:43:56 -0300

    Simple singup

--- login/models.py
+++ login/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Inpute_user(models.User):
+    pass
+
+    def __str__(self):
+        return 
 
     
+
--- login/views.py
+++ login/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 # Create your views here.
-def helloworld(request):
-    return render(request, 'login/cadastro.html')
+def cadastro(request):
+    if request.method() == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_passoword = form.cleaned_data.get('password1')
+            user = authenticate(username = username, password = raw_password)
+            login(request,user)
+            reuturn redirect('home')
+        else:
+            form = UserCreationForm()
+        return render(request, 'login/cadastro.html', {'form': form})
+
+def  cadastro(request):
+    return render(request, 'login/home.html')
