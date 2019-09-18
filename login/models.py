b542d52f77fb8a7ae25445a290d5506e39e7733d
Gabriel Borges <gabrielfr.borges@gmail.com>
2019-09-18 17:48:03 -0300

    user_ra e user_courses added to  Profile

--- login/models.py
+++ login/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User, Group
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    user_ra = models.CharField(max_length= 30)
+    USER_COURSES_CHOICES = (
+      (1, 'student'),
+      (2, 'teacher'),
+      (3, 'secretary'),
+      (4, 'supervisor'),
+      (5, 'admin'),
+    )
 
+    user_course = models.PositiveSmallIntegerField(choices=USER_COURSES_CHOICES)
     
 
