75ca555931f0d2779685555f62a1e9eba0681157
Gabriel Borges <gabrielfr.borges@gmail.com>
2019-09-18 17:46:47 -0300

    user_ra e user_courses added to Cadastro

--- login/forms.py
+++ login/forms.py
@@ -5,9 +5,19 @@ from django.contrib.auth.forms import UserCreationForm
 class Cadastro(UserCreationForm):
     first_name = forms.CharField(max_length= 30, required= False, help_text= 'Opitional')
     last_name = forms.CharField(max_length= 30, required= False, help_text= 'Opitional')
-    email = forms.EmailField(max_length=254, required=True,
-    help_text='Required. Inform a valid email address.')
-    
+    email = forms.EmailField(max_length=254, required=True, help_text ='Required. Inform a valid email address.')
+    user_ra = forms.CharField(max_length= 30,  required= True, help_text='Required', label = 'RA', error_messages='')
+    USER_COURSES_CHOICES = [
+      (1, 'Analise e Desenvolvimento de sistemas'),
+      (2, 'Banco de dados'),
+      (3, 'Gestão da Produção Industrial'),
+      (4, 'Logística'),
+      (5, 'Manutafura Avançada'),
+      (6, 'Manutafura e Aeronaves'),
+      (7, 'Projetos de Estruturas Aeronáuticas')
+    ]
+    user_course = forms.ChoiceField(choices = USER_COURSES_CHOICES, widget = forms.RadioSelect, label= 'Curso')
+
     class Meta:
         model= User
         fields = (
@@ -17,4 +27,6 @@ class Cadastro(UserCreationForm):
         'email', 
         'password1', 
         'password2',
+        'user_ra',
+        'user_course',
         )
