cf674acfc27d7c237274c8db5996930f3d53dc79
Gabriel Borges <gabrielfr.borges@gmail.com>
2019-09-18 17:51:09 -0300

    Using Cadastro form view instead of UserCreationForm

--- login/views.py
+++ login/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from .forms import Cadastro
 # Create your views here.
+
 def cadastro(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = Cadastro(request.POST)
         if form.is_valid():
             form.save()
+            user.fresh_from_db()
+            user.profile.user_ra = form.cleaned_data.get('user_ra')
+            #user.profile.user_course = form.cleaned_data.get('user_course')
+            user.save()
             username = form.cleaned_data.get('username')
             raw_passoword = form.cleaned_data.get('password1')
             user = authenticate(username = username, password = raw_password)
             login(request,user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = Cadastro()
     return render(request, 'login/cadastro.html', {'form': form})
 
 def home(request):
